81ff4f457a7c018ec4886ccb29d00f2473aebd71
Swapnil <pawarswapnil3305@gmail.com>
2024-05-31 21:17:35 +0530

    c

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const userRouter = require('./routes/user.js');
 const { error } = require('console');
 
 // const dbUrl = process.env.ATLASDB_URL;
-const MONGO_URL = "mongodb://localhost:27017/wanderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
 .then(() => {
