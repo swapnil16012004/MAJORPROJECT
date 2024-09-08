2936eb11e7405f03db850514c5dbe7414511279c
Swapnil <pawarswapnil3305@gmail.com>
2024-09-08 23:18:55 +0530

    done

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const listingRouter = require('./routes/listing.js');
 const reviewRouter = require('./routes/review.js');
 const userRouter = require('./routes/user.js');
 const { error } = require('console');
-const listingController = require("./controllers/listings.js");
+
 
 const dbUrl = process.env.ATLASDB_URL;
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -85,17 +85,8 @@ app.use((req, res, next) => {
     next();
 })
 
-// app.get("/demouser", async(req, res) => {
-//     let fakeUser = new User({
-//         email : "student@gmail.com",
-//         username : "delta-student"
-//     })
 
-//     let registeredUser = await User.register(fakeUser, "helloWorld");
-//     res.send(registeredUser);
-// })
 
-// app.get("/listings/search", listingController.searchListing);
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
